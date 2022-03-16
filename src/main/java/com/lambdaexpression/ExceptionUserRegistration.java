42c903415d629c18377e14574932b5d334ca2173
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-03-17 01:04:54 +0530

    Using Lambda expression to validate user entry

--- src/main/java/com/lambdaexpression/ExceptionUserRegistration.java
+++ src/main/java/com/lambdaexpression/ExceptionUserRegistration.java
@@ -21,6 +21,8 @@ import java.util.regex.Pattern;
  * UC10- Junit Test for Happy as well as Sad test case.
  * UC11- JUnit Parameterised Test to validate multiple entry for the Email Address.
  * UC12- Refactor the Code to throw custom exceptions in case of Invalid User Details
+ * UC13- Refactor the Code to use Lambda Function to validate User Entry - Use Lambda
+ * Function to validate First Name, Last Name, Email, Mobile, and Password
  * @author user-Almas
  *
  */
